5a57557b8029b69bc211db9f2cb530b2bfa05ba8
Roberto Macias <robertomag7@hotmail.com>
2024-12-08 19:22:37 -0700

    refactor: buttons to search by region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
   public countries: Country[] = [];
   public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
     this.isLoading = true;
+    this.selectedRegion = term;
 
     this.countriesService.searchByRegion(term)
       .subscribe(countries => {
